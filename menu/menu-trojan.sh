39cc0ae2d1ea33056b07847b08b1390089b89431
kayshinko <166167053+kayshinko@users.noreply.github.com>
2024-04-06 15:37:09 +0700

    Update menu-trojan.sh

--- menu/menu-trojan.sh
+++ menu/menu-trojan.sh
@@ -23,7 +23,7 @@ green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 
 BURIQ () {
-  #  curl -sS https://raw.githubusercontent.com/kayshinko/Vivi/main/REGIST > /root/tmp
+    curl -sS https://raw.githubusercontent.com/kayshinko/Vivi/main/REGIST > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -41,7 +41,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-#Name=$(curl -sS https://raw.githubusercontent.com/kayshinko/Vivi/main/REGIST | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/kayshinko/Vivi/main/REGIST | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -58,7 +58,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
- #   IZIN=$(curl -sS https://raw.githubusercontent.com/kayshinko/Vivi/main/REGIST | awk '{print $4}' | grep $MYIP)
+   IZIN=$(curl -sS https://raw.githubusercontent.com/kayshinko/Vivi/main/REGIST | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
@@ -76,7 +76,7 @@ PERMISSION
 if [ "$res" = "Expired" ]; then
 Exp="\e[36mExpired\033[0m"
 else
-#Exp=$(curl -sS https://raw.githubusercontent.com/kayshinko/Vivi/main/REGIST | grep $MYIP | awk '{print $3}')
+Exp=$(curl -sS https://raw.githubusercontent.com/kayshinko/Vivi/main/REGIST | grep $MYIP | awk '{print $3}')
 fi
 ##
 MYIP=$(wget -qO- ipinfo.io/ip);
